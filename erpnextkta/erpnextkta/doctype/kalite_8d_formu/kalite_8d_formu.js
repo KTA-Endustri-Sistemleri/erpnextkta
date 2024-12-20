a5d64aec998c0ee6815b71ef57d14150a6ddf41b
Tufan Kaynak KTA <tufan.kaynak@framras.com>
2024-12-20 17:49:10 +0300

    Kalite 8D formu eklendi

---
+++ erpnextkta/erpnextkta/doctype/kalite_8d_formu/kalite_8d_formu.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Framras AS and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Kalite 8D Formu", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ erpnextkta/erpnextkta/doctype/kalite_8d_formu/kalite_8d_formu.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Framras AS and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Kalite8DFormu(Document):
+	pass
