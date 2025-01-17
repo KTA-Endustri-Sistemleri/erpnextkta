d8d80baf6e0120635198e124ab1c4b868c25b021
Tufan Kaynak KTA <tufan.kaynak@framras.com>
2025-01-17 12:38:07 +0300

    Antrepo Beyanname No

---
+++ erpnextkta/erpnextkta/doctype/kta_gumruk_beyanname_no/kta_gumruk_beyanname_no.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Framras AS and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("KTA Gumruk Beyanname No", {
+// 	refresh(frm) {
+
+// 	},
+// });
