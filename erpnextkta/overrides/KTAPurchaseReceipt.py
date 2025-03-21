158b99f63ab19dfb25089b1de450c620b80dfa20
ilketorun <ilketorun97@gmail.com>
2025-03-21 15:12:07 +0300

    refactor

--- erpnextkta/overrides/KTAPurchaseReceipt.py
+++ erpnextkta/overrides/KTAPurchaseReceipt.py
@@ -1,54 +1,57 @@
 import frappe
 from erpnext.stock.doctype.purchase_receipt.purchase_receipt import PurchaseReceipt
-
+from erpnext.stock.doctype.batch.batch import split_batch
+import socket
 
 class KTAPurchaseReceipt(PurchaseReceipt):
-    def on_submit(self):
-        if self.docstatus == 1 and self.is_return == 0:
-            errors = []  # List to collect error messages
 
-            for row in self.get("items"):
-                item_has_batch_no = frappe.db.get_value("Item", {"name": row.item_code}, "has_batch_no")
-                if item_has_batch_no == 1:
-                    split_qty = row.custom_split_qty
-                    if not split_qty or split_qty <= 0:
-                        errors.append(
-                            f"Row {row.idx}: custom_split_qty must be a positive number. Please set a valid value for custom_split_qty."
-                        )
+    def send_data_to_zebra(self, data, ip, port):
+        pass
 
-            # If there are errors, throw them as a single message
-            if errors:
-                frappe.throw("\n".join(errors))  # Combine errors with newline characters
+    def zebra_formatter(data):
+        pass
+        """
+            ^XA
+            ^FO50,50^A0N,40,40^FD*Batch No: 123456*^FS
+            ^XZ
+        """
 
-            # Proceed with posting and batch splitting if no errors
-            super().on_submit()
-            self.split_kta_batches(table_name="items")
-        else:
-            super().on_submit()
+    def print_zebra(self):
+        data =frappe.db.get_value(
+            "KTA Depo Etiketleri",
+            {"satınalma_irsaliyesi": self.name},
+            ["item_code", "qty", "uom", "goods_receipt_date", "sut"],
+            as_dict=True
+        )
+        
+        # description, item tablosundan alınacak
+        # self.supplier_delivery_note
+        
+        print(data)
+        # formatted_data = self.zebra_formatter(data)
+        # send_data_to_zebra(formatted_data, ip, port)
 
-    def split_kta_batches(self, table_name=None):
-        from erpnext.stock.doctype.batch.batch import split_batch
+    def custom_split_batch(self, row, batch_no, qty):
+        """Helper function to split a batch."""
+        batch = split_batch(
+            batch_no=batch_no,
+            item_code=row.item_code,
+            warehouse=row.warehouse,
+            qty=qty
+        )
 
-        def _split_batch(row, batch_no, qty):
-            """Helper function to split a batch."""
-            batch = split_batch(
-                batch_no=batch_no,
-                item_code=row.item_code,
-                warehouse=row.warehouse,
-                qty=qty
+        frappe.get_doc(
+            dict(
+                doctype="KTA Depo Etiketleri",
+                satınalma_irsaliyesi=row.parent,
+                malzeme=row.item_code,
+                qty=qty,
+                uom=row.stock_uom,
+                sut_barcode=batch
             )
-            # now we prepare our custom content for labels
-            frappe.get_doc(
-                dict(
-                    doctype="KTA Depo Etiketleri",
-                    satınalma_irsaliyesi=row.parent,
-                    malzeme=row.item_code,
-                    qty=qty,
-                    uom=row.stock_uom,
-                    sut_barcode=batch
-                )
-            ).insert()
+        ).insert()
 
+    def custom_split_kta_batches(self, table_name=None):
         for row in self.get(table_name):
             if row.serial_and_batch_bundle:
                 row_batch_number = frappe.db.get_value(
@@ -56,6 +59,7 @@ class KTAPurchaseReceipt(PurchaseReceipt):
                     {"parent": row.serial_and_batch_bundle},
                     "batch_no"
                 )
+
                 if not row_batch_number:
                     frappe.throw(f"Row {row.idx}: No batch number found for the item {row.item_code}.")
 
@@ -64,15 +68,32 @@ class KTAPurchaseReceipt(PurchaseReceipt):
                 remainder_qty = row.stock_qty % split_qty
 
                 if remainder_qty > 0:
-                    _split_batch(row, row_batch_number, remainder_qty)
+                    self.custom_split_batch(row, row_batch_number, remainder_qty)
 
                 # Use range(num_splits) to run the loop exactly num_splits times
                 for _ in range(num_splits):
-                    _split_batch(row, row_batch_number, split_qty)
+                    self.custom_split_batch(row, row_batch_number, split_qty)
 
-                sut_codes = frappe.db.get_value(
-                    "Batch",
-                    {"parent_batch": row_batch_number},
-                    ["batch_id", "batch_qty", "stock_uom"],
-                    as_dict=True
-                )
+    def verify_batch(self):
+        errors = []
+
+        for row in self.get("items"):
+            item_has_batch_no = frappe.db.get_value("Item", {"name": row.item_code}, "has_batch_no")
+            if item_has_batch_no == 1:
+                split_qty = row.custom_split_qty
+                if not split_qty or split_qty <= 0:
+                    errors.append(
+                        f"Row {row.idx}: custom_split_qty must be a positive number. Please set a valid value for custom_split_qty."
+                    )
+
+        if errors:
+            frappe.throw("\n".join(errors))
+        
+    def on_submit(self):
+        if self.docstatus == 1 and self.is_return == 0:
+            self.verify_batch()
+            super().on_submit()
+            self.custom_split_kta_batches(table_name="items")
+            self.print_zebra()
+        else:
+            super().on_submit()
