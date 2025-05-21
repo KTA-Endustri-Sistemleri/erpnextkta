ee344081e85d38fa6db6cb5bcee1150cd9425b25
Tufan Kaynak KTA <tufan.kaynak@framras.com>
2025-05-21 08:00:03 +0300

    BOM to Item set custom index

--- erpnextkta/overrides/KTAPurchaseReceipt.py
+++ erpnextkta/overrides/KTAPurchaseReceipt.py
@@ -3,6 +3,7 @@ from erpnext.stock.doctype.purchase_receipt.purchase_receipt import PurchaseRece
 from erpnext.stock.doctype.batch.batch import split_batch
 import socket
 
+
 class KTAPurchaseReceipt(PurchaseReceipt):
 
     def send_data_to_zebra(self, data, ip, port):
@@ -15,12 +16,10 @@ class KTAPurchaseReceipt(PurchaseReceipt):
             frappe.log_error(f"ZPL Print Error {str(e)}", "Printer Error")
             return {"status": "error", "message": f"Failed to send label {str(e)}"}
 
-
     def zebra_formatter(self, data):
         return f"""
             CT~~CD,~CC^~CT~
             ^XA
-            ^POI
             ~TA000
             ~JSN
             ^LT0
@@ -30,7 +29,7 @@ class KTAPurchaseReceipt(PurchaseReceipt):
             ^PMN
             ^LH0,0
             ^JMA
-            ^PR4,4
+            ^PR5,5
             ~SD15
             ^JUS
             ^LRN
@@ -39,62 +38,75 @@ class KTAPurchaseReceipt(PurchaseReceipt):
             ^XZ
             ^XA
             ^MMT
-            ^PW812
-            ^LL448
+            ^PW799
+            ^LL480
             ^LS0
-            ^FO9,29^GB409,747,1^FS
-            ^FO9,512^GB407,0,1^FS
-            ^FO94,33^GB0,743,1^FS
-            ^FO163,190^GB0,582,1^FS
-            ^FO205,188^GB0,588,1^FS
-            ^FO250,190^GB0,586,1^FS
-            ^FO302,190^GB0,586,1^FS
-            ^FO360,190^GB0,586,1^FS
-            ^FO360,33^GB0,158,1^FS
-            ^FO113,518^GFA,477,1512,6,:Z64:eJzVlDtuw0AMRCmo2FJHUI6QAwSQj5KbSIAvtkaKlGlTbudW5RaCmeFwZSn+wDaSFFnYr9jPkBzS7lSjSKeayQOpw4qxEKtLxpZszuj7uLN+daJ2WMWi5N2rgYCI6ihSq04igWoNlBGa+s5uFIvrH2xe5EmG1/lQhnIrw6LMDPtoXvWD+dbTw2/0fd4pFfGt67im63ssj1tyuLKe8N3IM55EecFdaIq7VJl+CsytNU6zA3O/EDcxoljEyStlv9mXxk5xPMrfePvDxVgNybhWo1TMoTVWR2/jMp/k3jgFYyQ1rE4LjxW5jrXHovRk7ZW2ccnk9nq1i3WqoB/GCl1ux1qdmKJsml0O+jH0CX2INi0g+2DMM7Gfk83PZ2KGifPjfHh+bG0MmB/khIqCDsik0Te6t7/QZfjzTpe2/J/ZIiA4kXnhr8zMv+syfzqhGJ+ZMZ0pPCzckcMZ7/btC1OWGlE=:7B5F
-            ^FO171,585^GFA,253,740,4,:Z64:eJzNks0NwyAMhV/EgUuljOBRskrH6A026SoeIEtkA44cUNwHNFIqpal6qQr6hGTjH54RM8gBpysoBkvwVjDa2mhxGiBR8LK/yu+IEPo9mYx1tpjcSQTPc7PR76zfb3E1vuVx5+/4uH7Uj17hNMMr9VTquVDPpel5o4KZFDJT25m2mXpXLlV/x3p1FgjxIPFAPFwS+BIwlv0MCmRKEFESCTp/OK8h89oK/rPaXyV27mTc8ab3B76pzt0=:5CEA
-            ^FO213,597^GFA,185,519,3,:Z64:eJytkcENgCAMRWs4cOwIHYXRYANGcBTZwCFcwMQLB0It6kGNojH+vAMNP/wPgA0Nj4qj5oScBQqWnCGgDVkHu26JR5ziBxPgRsS8B3QGTGBGKEFunc6eryr1Kjqm1IJ8VD5qn7DPVGDyHWFbqD2Cuy51xcsyP90oqiHhtPzm5nw4ZwaeBJwK:AD78
-            ^FO258,593^GFA,225,531,3,:Z64:eJylkbENAyEMRX2ioGQEbxJWySYwmlc5ZQGkNBQIxwak3AGp8vWFMDyMMRDo4GQ4Wy6OqxgpYPQIOCxzCn1LGCGFB0/wQ8h8NdgKroBPoBfFHs3Mf5qyacIHXL0BtqKnoWypOKrurHiytuLbB9z0IcS5mNXL7Stzl2lr0joGz8c4ldUpI7Sxh8yGlVFSeD1llldl8yru3X52pMKblvI+7LSx7A==:C425
-            ^FO306,605^GFA,325,990,6,:Z64:eJytkzEOwjAMRR0xZOwRyhEYmQg3ayUGrpWt10BiYM3YIcLY34GoQ2kryPAUpfG38/VLJOuscJFcpiaS5z5EajhypJbv4EMYmAtJmYUDJ+FFjoVPUmZwrPxUrePywh0PdsodqoLSQaFVOqg1NifzWHlVZq+Mvu55GKc3rdZ0TFNa1V6lr81Q5qH5+UO/kRt926o/t/akeTggD0e86ySvF328NESnvW5emVpl7rgveRCTEnwzq0jdG+FRRmNNiHzW/h6Z+Xs2pgtV3Vf+rr/EZDeN5Y8wf9QUyxUZfd3jfPdOb6y10Ekru78AnpNp/g==:AED9
-            ^FO362,643^GFA,217,774,6,:Z64:eJytkTEOwyAMRY06MHKEHMW9WSIx9FrecpAOXTsyoPxgEwllaBVXZXhiwO9/GWIAQtdJsRBdotvsPDY1f+VP/rsiCIVKSShiYaEEaYYJT+PrZLYmtXHFuzFrFOdNG+ZqLIN/34m9icZZebMpVgYzTMpgttR7AmXwoaz6YSxx3LGW88s+2z3d2aJG1pHbOxx96HN/Xpx07s3p3wFfZhrk:F409
-            ^FO32,53^GFA,393,2260,5,:Z64:eJzVlU1ugzAQhWfEgqWP4N6Eq+QksXe9Fl31GHVvwK5EtTydZ0OlJoqbEgSpJT5ZMPPmx2PBIhL4Gqi6eKyiqlz3vbaeYBDIBhPIjV0giZrkD2XP0UVV0Y8KD9CE5aUe9DEwgZ1RMDzs5NsrIk14UUTG7v37XcHkUXyzStEryjnGZYtRNPdatJbdomtd4ESI2wJvc9GIQQ5yVjxRix3LoO4u3lP5mquShs9fMxJRk7NqZzTFLs0mviK1Qi4POPcrJX6rFNreY5Ioj48ewICB9USdng7JuPQADrTXPdrzyBYulceNP/4BG8zGtnd1hx6UkAkQD/TAAEQgKT7lOfBJXjWk+1BYTIkBmjwvGQ6wCGl+j2vCDfiPf7Dz7m4wV18GtT2K:FC8D
-            ^FO90,47^GFA,209,696,6,:Z64:eJylkdEJwzAMROXqo58ZwaNktASyWKCLdIR8Gmqi6C7BpaXEDhX4IWPrTrJFF4mj6JKv0cxqPA/3rbPBpUWn0gl0wus3m2Yp0Y8X+P8blgi8GcHAqggGKvRmnDG9OYFJmT/S5+l+H5uic2ju+ofXV9x8ec9e55ylcz5lcCbUar5zoriyKfzJYDO8VhR3GVTmtGp4mQ3qDs6e:DFC6
-            ^FO138,63^GFA,217,450,5,:Z64:eJx9kDEOwzAIRXEYOvYIPkqOVqRerFIvlJHBCv0f2jSV0jA82QaejSdBqBELMYhIPERaOJa3CJMroRtyy0SWZHG1jU1F6ST/o7GgG28BOtAonSN4hX9wB1y5evoukXX1luwtS/rKfBxoCDsCLZx10IT3XzjS79FJ72lo2Rf+JFwRKyeNgq4Ci8ts+LW+R54x+y3OXkzubx/NL6nTnEk=:6380
-            ^FO371,121^GFA,137,244,4,:Z64:eJxj/v//ATMIf2BgYPzfwMDA/4+BgcH+AZB4AKbZ/4HFQfLMULUMuEECNkyEPgaGAnsGhg/1DYwHHh5gZjyQAMbMDWDM2M7wgPE/4wGGGn6gA/kwtDJjwzB/YZEDABYgMNM=:290D
-            ^FO360,372^GB55,0,1^FS
-            ^FO376,200^GFA,289,498,3,:Z64:eJy1UDuuAyEMHB5Stnuk3CJaXyTi3WyXLmVukFwlRyE3IB0FgjfeaPNvY40QtmewGWlVFsAUdBku4i9gumfyyHmPKZgWu5YJ14o7FQmjwN8RRhbZIsG2TDIlN/Ua6IEBkAA5QRIc57ZqOGvSk3dWWNduUGY/q57CRrPL3b444ljlMMrgFdsZZXSluly6VGzMQFxkK8DDVDidZfWDSXFOYpJc0jW18ybKIZN8Vd2W/+nxu4EfUMVW2fMFmkA3gr3SJ9DMRwO/4eES/iNepv8Dr3atUw==:5A94
-            ^FO95,189^GB323,0,1^FS
-            ^FO181,29^GB0,161,1^FS
-            ^BY2,3,39^FT140,450^BCB,,Y,N,,A
-            ^FD{data.item_code}^FS
-            ^FPH,3^FT190,504^A@B,22,22,TT0003M_^FH\^CI28^FD{data.item_name}^FS^CI27
-            ^FPH,3^FT234,507^A@B,22,22,TT0003M_^FH\^CI28^FD{data.supplier_delivery_note}^FS^CI27
-            ^FPH,6^FT352,507^A@B,32,31,TT0003M_^FH\^CI28^FD{data.qty}^FS^CI27
-            ^FPH,3^FT399,507^A@B,22,22,TT0003M_^FH\^CI28^FD{data.gr_posting_date}^FS^CI27
-            ^FPH,3^FT342,341^A@B,22,22,TT0003M_^FH\^CI28^FD{data.uom}^FS^CI27
-            ^BY2,3,26^FT279,454^BCB,,Y,N,,A
-            ^FD{data.sut}^FS
-            ^PQ1,,,Y
+            ^FO23,25^GB747,409,1^FS
+            ^FO286,25^GB0,407,1^FS
+            ^FO23,110^GB743,0,1^FS
+            ^FO27,179^GB582,0,1^FS
+            ^FO23,221^GB588,0,1^FS
+            ^FO23,266^GB586,0,1^FS
+            ^FO23,318^GB586,0,1^FS
+            ^FO23,376^GB586,0,1^FS
+            ^FO608,376^GB158,0,1^FS
+            ^FO27,130^GFA,533,1344,32,:Z64:eJy11DtywyAQBuBVKCh1g+goOhqacZHSV2Imha9BJkXKkA5PMJt/FyRLflZRYaH9YEGCNdFy5XOTytwYmYi94UwGLReI+jBGw5PhQK7LI0/EwXChnj25KO6ClSGpulfntfteQrl6qD4NklCd/cBRUm585Fj9Q9pJUlbvpDN7ccx55bG52zf/BByypBTfq6fRO5uaY+JdNsmpv8XuFz6on8S/dGEYsnhSN4FOWTxv3VQvhjr174TPW3TI2UePT16dL/0oL6Oe1ePa93B9GfVy5Xi5n8VTucrPwarLlpv7Xh54LLKZ6k48b9w/8clyMUdEXkiWAS9rl4OFB6zPeKteNj5Vd4jE5nzprJtpExyP2bX9uePJrfevOR5snH1q52PcIU6DupyvXj2ibz1f46E5+sn5hHupH9/OJ27iHnnww6EPg/gwO8pg8TCcPbT62Djqa/FWX7jBMd176hGbMDDA+7k+146ENPtc37jRw/+Hsgp1a79xdeWx0+v/un0yv32yfhtvRf8AzO4Xvw==:CA55
+            ^FO27,190^GFA,321,648,24,:Z64:eJyV0UFuwyAQBdAfecEm0hyBo/hKPQHmZC1HIcoFnJ2lIKafwS6xmi7K6vNkf+NhrnAFS8W0tQAsyWl0eVZzhdThuXk0j16Hr+bSPPUA6OGJDXS+wkDPTje6T/KJIGmxADyzuzeX3evwWzH3Xwg+lSsYcHmyvFgPt+znN0+uyft2zuae/jg8zj52Z8D02F694pdj9+vZ7WfDgiJsS3BausN8jjzq4bfh/uSbuWo2jAwQrX0+3aXN88dXc7YFyZw/AySF3S/FrcFlqS2Ahf2+dl+nbTjvF8d6iafFZ9+vj/+5/NEj6bT9Bjhv8OY=:BC39
+            ^FO27,232^GFA,249,462,22,:Z64:eJxt0cENwyAMBVAjDhwZIWv05s0C3YxuQjZwbqmU4rp2nTRST0gPCX3+xwHIbQaYCJjlCARYkatp2UzvctsP5d30QYFJSDXwcI28uUbXhRLvmBsCiG4FdjBdT6Wb6UqpD9fUwfRJuc2aoZrK04eC6cKmr7/Kpu2q5a+iauqWQVQzXDQSmk6UJa+r/y3r3z61tTKilKA9ZO3hq8E1nZ2VcfSbrN9q6ltE3UJ3E536z25vbabVlg==:A092
+            ^FO27,277^GFA,265,504,24,:Z64:eJxt0csNwyAMBmBHHHKJxAgehc0CG3SErsImYQSqHppDZNeOoU2i3MwnXv4dCBxB5OJWmH2GoSJICYGTuSfzFUHKvyObbwhSiudxd9nQXcrdWT02J5RVhuDzuMitEDH/HA9OzRnlnaLulyS+Tfv5gZ+wjUXfaA4XB/84+dIdH/no3Dyf/XP1yfp9Nx+l06Lu063P7f8vBvI3Xtn61WSq5tDyUeeD9zxr7HnOTr3nL275J3KreJ9XDTYvAvPLfL82qthY:EBB9
+            ^FO27,323^GFA,321,984,24,:Z64:eJzN0kFuAyEMBVCjWbDkCNykXKwVk5N15iZEuQBdhajILvaE4ETTSNnFrHiWLPMFAIDJsFvv5q9UyL5YPuiJEtBsiLS7OnxR7gu7ocVwW3kezuuFEsTDPx4T+8QtKtqXfW9L+ATs6JRfaBY/N7fiNW4O7Pacu39+sf/ivpNyp72Ku1Pu+z946f5xeerbfHcsPbc792vtOV+dlMMz9z/i35KDp2owzsqDeCA0GJbhfOnutXvlaXjl4QkiUff2FPknw13WnvfdDt/y717YCeLh6nYVncQRwgGnO6/D66Ob5isa7Y1u3jKHPx84Z84=:1DCA
+            ^FO27,379^GFA,209,820,20,:Z64:eJzF0LENwyAUBNCPXLj8IzAKo8FmZhRG+FEaCsSFWMgBG9l0ufIVp9MRHWFHl/zLbgOA1kSMSMYbb11ruTfOxTAy92gaX/O3RqDdxJ5sK2Z6M5ubt72vLKpmtav7WvNzxr5+8LPMoX51svhsQOB3onazAoRf+WoyMnS2AHFs1s1Z/98al3QxUcX0yEJrLCqz8MjksA+mShZs:443B
+            ^FO292,55^GFA,525,1596,57,:Z64:eJyd1T1SxCAUwPGXSUGZI7yLOHIsmwhYeQxvouxs4TXiWKRTuo0jw5NHPnYh0ahd8h9++SAwkUcP6KoBGnJInTz6igKApk54aIVFQ2R97aWFapActQdO8ieoSmgZ0t+hHqHZhbqEJody2IJNWEECjmT34f0vIIgODTCMx8BXmmDtYIIfgaNCy0n2Hq4TbBLsz/AR9PMMg57gI6hmgSoNRptDfCrhp+f4HzhcwOu+naA0SDaeVuQX2M/w7SZCGiGeYZsgFPBhBV2KtMA2QV1CBH1a3RF5AczQK4ahgBLNGnKcIOWQLuEtzfD9xNCPEDJY+d/CsEAKDOMLGGA4fQ5Jpl1DWmCcUqI4rh6hWmCcvTPse4ZpoSqdQ+E2oM6hSlAWsNuHeoQmg80+DDJBnGFNdEcD2gISda3MoJcc5yW3wLSR0wOEb6HJocMt6EXXYgYHNDHOu+Mqh1fb8PU5QZtgtw+bH6FweFjDygtXwuYQoxJn2GzC2qVfwCV8iVEJx+kLCbb6KQ==:56D5
+            ^FO634,113^GFA,193,544,16,:Z64:eJytz7ENwjAQheEXXXGlN8CjeDGUZAM2YBKEzCR4BJdXWD4IEOnORSr+7queHvCNMlz/9l4iIcEMLqoPbUhcBxfvMDhmbs6rd9pNdZL3XgLfrGcE545wt27GKh9fB5+sO2I8sg5+Iqrdu6xJ1wOH7M2bl268aP55+09l9Jmdq/dUGxdrETJ+ARtzvs8=:19B1
+            ^FO645,160^GFA,221,324,12,:Z64:eJxl0MsJwzAMBmAFH3TUBvUm1mIlMXSAbNBJQgl0EUEXMPRiqKkquw5J6e27/Y+hQHDi8pBRUIGrodBKGg9e+VI9+uibsVSzCt83z93MwCj8EFyqJ0xmWsC/YXSJn0K3H1NpfgldDz4B5mYV77vDn11qnsTrbtZottzA3dZn9yzBb9bqSVez7QrUTc1n/DoG7LZ/zAXFPLTfsqv+AE+Rjpk=:1587
+            ^FO617,393^GFA,137,168,8,:Z64:eJz7/4GfwYDhgcX/D/YNBgwfKtgYDA5A6IQHUDoBjQaqv3CDjaEASN8A0jZQ2h5K8zdAaGagOSdmsDEwAM05C6ITIPQBHDTIvqMSQHOB+o4DafsG+wMgWp4BRAMAOHI1Qw==:0E99
+            ^FO426,376^GB0,55,1^FS
+            ^FO431,395^GFA,325,420,21,:Z64:eJxV0TFOxDAQBdCxXKScI/gm64utNl6loMwROAqWKCj3BpAVBeUO2oIprAx/4oCgsvwk23++D0SZ+NxCo1Ry0CgkfCCyypMCxyVNwo0UGEz4QYBYX5Zk1IARm3mJa9gwdxxM+bE66nCro5XV8Qos8TWI4yltyLXxEw1vQXBTaVzNsRzZHBveJB2qzb94QbSOXz9YeC6j0Lrh5Q9m7Xi/7ViBsdHR8XPHj5qsgjpaj+TjPS+OiCTWw0+O4scRXsa8+piTZHs3R4zp6IWcgVdTIAqRvKMe4j/0koueopgAUbIkIL6jNGD2UNt3fANWm8cy:DF27
+            ^FO609,111^GB0,323,1^FS
+            ^FO609,197^GB161,0,1^FS
+            ^BY2,3,39^FT349,156^BCN,,Y,N
+            ^FH\^FD>:{data.item_code}^FS
+            ^FPH,3^FT295,206^A@N,22,22,TT0003M_^FH\^CI28^FD{data.item_name}^FS^CI27
+            ^FPH,3^FT292,250^A@N,22,22,TT0003M_^FH\^CI28^FD{data.supplier_delivery_note}^FS^CI27
+            ^FPH,6^FT292,368^A@N,32,31,TT0003M_^FH\^CI28^FD{data.qty}^FS^CI27
+            ^FPH,3^FT292,415^A@N,22,22,TT0003M_^FH\^CI28^FD{data.gr_posting_date}^FS^CI27
+            ^FPH,3^FT458,358^A@N,22,22,TT0003M_^FH\^CI28^FD{data.uom}^FS^CI27
+            ^BY2,3,26^FT345,295^BCN,,Y,N
+            ^FH\^FD>:{data.sut}^FS
+            ^PQ1,0,1,Y
             ^XZ
         """
 
     def print_zebra(self):
+        # Get the current logged-in user
+        user = frappe.session.user
+
+        # Query the printer for this user that is both enabled and marked as default
         printer = frappe.db.get_value(
             "KTA User Zebra Printers",
-            {"disabled": 0},
-            ["item_code", "qty", "uom", "sut", "item_name", "supplier_delivery_note", "gr_posting_date"],
-            as_dict=True
+            {
+                "user": user,
+                "disabled": 0
+                #        "is_default": 1  # Assuming there's a field to mark default printers
+            },
+            ["printer"]
         )
+        if printer:
+            zebra_printer = frappe.get_doc("KTA Zebra Printers", {"printer_name": printer})
+            ip_address = zebra_printer.ip
+            port = zebra_printer.port
 
-        data = frappe.db.get_value(
-            "KTA Depo Etiketleri",
-            {"gr_number": self.name},
-            ["item_code", "qty", "uom", "sut", "item_name", "supplier_delivery_note", "gr_posting_date"],
-            as_dict=True
-        )
-        # "10.41.10.23", 9100
-        formatted_data = self.zebra_formatter(data)
-        self.send_data_to_zebra(formatted_data, "10.41.10.23", 9100)
+            # Example usage:
+            frappe.msgprint(f"Printer IP: {ip_address}, Port: {port}")
+            # printer will now contain ip_address and port if found, or None if not
+            for data in frappe.get_all("KTA Depo Etiketleri", filters={"gr_number": self.name},
+                                       fields={"item_code", "qty", "uom", "sut", "item_name", "supplier_delivery_note",
+                                               "gr_posting_date"}):
+                # "10.41.10.23", 9100
+                formatted_data = self.zebra_formatter(data)
+                self.send_data_to_zebra(formatted_data, ip_address, port)
+        else:
+            frappe.msgprint("No default printer found for the current user.")
 
     def custom_split_batch(self, row, batch_no, qty):
         """Helper function to split a batch."""
@@ -163,8 +175,7 @@ class KTAPurchaseReceipt(PurchaseReceipt):
                     self.custom_create_packages(row, row_batch_number, split_qty, pack)
 
                 if remainder_qty > 0:
-                    self.custom_create_packages(row, row_batch_number, remainder_qty, num_packs+1)
-
+                    self.custom_create_packages(row, row_batch_number, remainder_qty, num_packs + 1)
 
     def verify_batch(self):
         errors = []
@@ -180,14 +191,14 @@ class KTAPurchaseReceipt(PurchaseReceipt):
 
         if errors:
             frappe.throw("\n".join(errors))
-        
+
     def on_submit(self):
         try:
             if self.docstatus == 1 and self.is_return == 0:
                 self.verify_batch()
                 super().on_submit()
                 self.custom_split_kta_batches(table_name="items")
-                # self.print_zebra()
+                self.print_zebra()
             else:
                 super().on_submit()
         except Exception as e:
