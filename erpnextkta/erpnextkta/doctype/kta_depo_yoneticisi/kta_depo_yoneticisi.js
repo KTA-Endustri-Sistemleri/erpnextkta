23ee570d305a717f19514d909ee681f6520468ff
Tufan Kaynak KTA <tufan.kaynak@framras.com>
2025-07-15 18:03:24 +0300

    özel alanlar

--- erpnextkta/erpnextkta/doctype/kta_depo_yoneticisi/kta_depo_yoneticisi.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Framras AS and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("KTA Depo Yoneticisi", {
-// 	refresh(frm) {
-
-// 	},
-// });
