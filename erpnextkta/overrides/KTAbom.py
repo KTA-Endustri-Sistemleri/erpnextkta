cfe0ab4e6a2228be3b1ea4c855bcd29d49b18188
alpkan <alpkan.ozturk@kta-endustri.com.tr>
2025-08-14 16:23:49 +0300

    hata giderme

--- erpnextkta/overrides/KTAbom.py
+++ erpnextkta/overrides/KTAbom.py
@@ -6,10 +6,8 @@ class KTAbom(BOM):
     def manage_default_bom(self):
         super().manage_default_bom()
         if self.is_default == 1:
-<<<<<<< HEAD
+
             frappe.db.set_value("Item", self.item, "custom_musteri_indeksi_no", self.custom_musteri_indeksi_no)
-=======
-            frappe.db.set_value("Item", self.item, "custom_musteri_indeksi", self.custom_musteri_indeksi_no)
->>>>>>> 8a9f6af (alpkan gelistirmeler calisma karti + mrp)
+
         else:
             frappe.db.set_value("Item", self.item, "custom_musteri_indeksi_no", None)
