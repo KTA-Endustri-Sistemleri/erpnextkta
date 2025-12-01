129b91b8db94c2d662f863db27466db001e80700
Ufuk Karamalli <ufukkaramalli@gmail.com>
2025-12-01 12:41:59 +0000

    feat(kta_calisma_karti_operasyonlari): add KTA Calisma Karti Operasyonlari doctype with fields and functionality

--- erpnextkta/erpnextkta/doctype/kta_calisma_karti_operasyonlari/test_kta_calisma_karti_operasyonlari.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Framras AS and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestKTACalismaKartiOperasyonlari(FrappeTestCase):
-	pass
