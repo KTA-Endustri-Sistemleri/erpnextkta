8681eb8299912b70709ed6a86fb930be71ed153a
Ufuk Karamalli <ufukkaramalli@gmail.com>
2025-12-03 00:19:39 +0000

    chore: title default changed to null

--- erpnextkta/kta_calisma_karti/page/kta_calisma_karti/kta_calisma_karti.js
+++ erpnextkta/kta_calisma_karti/page/kta_calisma_karti/kta_calisma_karti.js
@@ -1,7 +1,7 @@
 frappe.pages['kta_calisma_karti'].on_page_load = function (wrapper) {
   const page = frappe.ui.make_app_page({
     parent: wrapper,
-    title: 'Barkod ile Çalışma Kartı Oluşturma',
+    title: null,
     single_column: true,
   });
 
