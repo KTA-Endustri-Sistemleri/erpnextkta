7aa9d2ce438431f31866aadaf9fe2863833ad225
Ufuk Karamalli <ufukkaramalli@gmail.com>
2025-12-06 18:26:28 +0300

    chore: configure python-semantic-release with pyproject version (#7)

--- erpnextkta/__init__.py
+++ erpnextkta/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.0.1"
+from importlib.metadata import version as _version, PackageNotFoundError
+
+try:
+    __version__ = _version("erpnextkta")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
