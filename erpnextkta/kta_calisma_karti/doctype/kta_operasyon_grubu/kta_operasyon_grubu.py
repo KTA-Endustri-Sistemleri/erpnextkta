3fe23cc3d5ca7d7c1a73a356f10d5440f173c7d3
alpkanoz <ozturk.alpkan@gmail.com>
2025-12-08 13:01:06 +0300

    refactor: KTAOperasyonGrubu class and remove unused KTASalesOrderUpdateEntry class

--- erpnextkta/kta_calisma_karti/doctype/kta_operasyon_grubu/kta_operasyon_grubu.py
+++ erpnextkta/kta_calisma_karti/doctype/kta_operasyon_grubu/kta_operasyon_grubu.py
@@ -5,11 +5,5 @@
 from frappe.model.document import Document
 
 
-<<<<<<<< HEAD:erpnextkta/kta_calisma_karti/doctype/kta_operasyon_grubu/kta_operasyon_grubu.py
 class KTAOperasyonGrubu(Document):
 	pass
-========
-class KTASalesOrderUpdateEntry(Document):
-    """Child rows for KTA Sales Order Update; no custom server logic yet."""
-    pass
->>>>>>>> 174b968 (feat: Remove unused KTA Supply On Entry and KTA Supply On Evaluation doctypes):erpnextkta/kta_sales/doctype/kta_sales_order_update_entry/kta_sales_order_update_entry.py
