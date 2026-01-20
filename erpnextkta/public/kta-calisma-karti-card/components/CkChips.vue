c4bf5dedf00c920e007e966a3acf23a5445d80bf
Ufuk Karamalli <ufukkaramalli@gmail.com>
2026-01-20 17:01:59 +0000

    refactor(calisma-karti): add CkActionbar, CkChips, CkTabs, and CkTopbar components

---
+++ erpnextkta/public/kta-calisma-karti-card/components/CkChips.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  durumLabel: string;
+  statusClass: string;
+  qcLabel: string;
+  qcClass: string;
+}>();
+</script>
+
+<template>
+  <div class="ck-chips">
+    <span :class="['ck-status-badge', props.statusClass]">{{ props.durumLabel }}</span>
+    <span :class="['ck-chip', props.qcClass]">QC: {{ props.qcLabel }}</span>
+  </div>
+</template>
