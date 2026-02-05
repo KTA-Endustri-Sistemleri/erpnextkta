967d81efb3524da7a132687862ee831979b0a32c
Ufuk Karamalli <ufukkaramalli@gmail.com>
2026-02-06 01:42:29 +0300

    feat(kta-calisma-karti): new calisma karti user interface and improvements

---
+++ erpnextkta/public/kta-calisma-karti-card/components/CkActionbar.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+const props = defineProps<{
+  showStart: boolean;
+  showResume: boolean;
+  showStop: boolean;
+  showFinish: boolean;
+  qcApproved: boolean;
+  onBaslatDevam: () => void;
+  onDurus: () => void;
+  onBitir: () => void;
+}>();
+</script>
+
+<template>
+  <div class="ck-actionbar">
+    <button
+      v-if="props.showStart"
+      class="ck-btn ck-btn--success ck-btn--wide"
+      @click="props.onBaslatDevam"
+    >
+      Başlat
+    </button>
+
+    <button
+      v-if="props.showResume"
+      class="ck-btn ck-btn--success ck-btn--wide"
+      @click="props.onBaslatDevam"
+    >
+      Devam Et
+    </button>
+
+    <button
+      v-if="props.showStop"
+      class="ck-btn ck-btn--warning ck-btn--wide"
+      @click="props.onDurus"
+    >
+      Duruş
+    </button>
+
+    <button
+      v-if="props.showFinish"
+      :disabled="!props.qcApproved"
+      class="ck-btn ck-btn--danger ck-btn--wide"
+      @click="props.onBitir"
+    >
+      Bitir
+    </button>
+  </div>
+</template>
