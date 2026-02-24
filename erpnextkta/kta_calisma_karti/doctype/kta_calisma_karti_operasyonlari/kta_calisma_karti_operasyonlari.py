f5e972c6276c334819c15d6b084ad752e3a7f013
Ufuk Karamalli <ufukkaramalli@gmail.com>
2026-02-24 14:42:40 +0000

    feat(kta-calisma-karti): enforce composite unique constraint on operation + customer group + plant floor

--- erpnextkta/kta_calisma_karti/doctype/kta_calisma_karti_operasyonlari/kta_calisma_karti_operasyonlari.py
+++ erpnextkta/kta_calisma_karti/doctype/kta_calisma_karti_operasyonlari/kta_calisma_karti_operasyonlari.py
@@ -1,9 +1,25 @@
-# Copyright (c) 2025, Framras AS and contributors
-# For license information, please see license.txt
-
-# import frappe
+# -*- coding: utf-8 -*-
+import frappe
 from frappe.model.document import Document
-
+from frappe import _
 
 class KTACalismaKartiOperasyonlari(Document):
-	pass
+    def validate(self):
+
+        if not (self.calisma_karti_op and self.customer_group and self.plant_floor):
+            return
+
+        filters = {
+            "calisma_karti_op": self.calisma_karti_op,
+            "customer_group": self.customer_group,
+            "plant_floor": self.plant_floor,
+        }
+
+        existing = frappe.db.exists(self.doctype, filters)
+        if existing and existing != self.name:
+            frappe.throw(
+                _("Bu kombinasyon zaten var: {0} / {1} / {2} (Kayıt: {3})").format(
+                    self.calisma_karti_op, self.customer_group, self.plant_floor, existing
+                ),
+                frappe.DuplicateEntryError,
+            )
