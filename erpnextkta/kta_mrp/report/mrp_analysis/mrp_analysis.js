bcc7d325314e7cb6ff6c89097cb47c85b5958159
Alpkan Öztürk <78761783+alpkanoz@users.noreply.github.com>
2026-04-03 14:10:43 +0300

    feat: add MRP Analysis report to visualize material consumption by customer group and stock levels (#118)

---
+++ erpnextkta/kta_mrp/report/mrp_analysis/mrp_analysis.js
@@ -0,0 +1,90 @@
+frappe.query_reports["MRP Analysis"] = {
+	onload: function (report) {
+		// Ara Malzeme Grubu seçeneklerini dinamik doldur
+		frappe.call({
+			method:
+				"erpnextkta.kta_mrp.report.mrp_analysis.mrp_analysis.get_ara_malzeme_gruplari",
+			callback: function (r) {
+				const field = report.get_filter("ara_malzeme_grubu");
+				const options = r.message || [];
+				field.df.options = ["", ...options];
+				field.refresh();
+			},
+		});
+
+		// Hammadde Grubu (Item Group) seçeneklerini dinamik doldur
+		frappe.call({
+			method:
+				"erpnextkta.kta_mrp.report.mrp_analysis.mrp_analysis.get_item_groups",
+			callback: function (r) {
+				const field = report.get_filter("item_group");
+				const options = r.message || [];
+				field.df.options = ["", ...options];
+				field.refresh();
+			},
+		});
+	},
+
+	filters: [
+		{
+			fieldname: "ara_malzeme_grubu",
+			label: __("Ara Malzeme Grubu"),
+			fieldtype: "Select",
+			options: [],
+		},
+		{
+			fieldname: "musteri_grubu",
+			label: __("Müşteri Grubu"),
+			fieldtype: "MultiSelectList",
+			get_data: function (txt) {
+				return frappe.db.get_link_options("KTA Customer Group", txt);
+			},
+		},
+		{
+			fieldname: "item_group",
+			label: __("Hammadde Grubu"),
+			fieldtype: "Select",
+			options: [],
+		},
+		{
+			fieldname: "varsayilan_tedarikci",
+			label: __("Varsayılan Tedarikçi"),
+			fieldtype: "Link",
+			options: "Supplier",
+		},
+		{
+			fieldname: "sifir_tuketimi_goster",
+			label: __("Sıfır Tüketimi Göster"),
+			fieldtype: "Check",
+			default: 0,
+		},
+	],
+
+	formatter: function (value, row, column, data, default_formatter) {
+		if (column.fieldtype === "Float") {
+			if (!value || value === 0) {
+				return "";
+			}
+
+			return parseFloat(value).toLocaleString("tr-TR", {
+				minimumFractionDigits: value % 1 === 0 ? 0 : 2,
+				maximumFractionDigits: 6,
+			});
+		}
+
+		if (column.fieldtype === "Percent") {
+			if (!value || value === 0) {
+				return "";
+			}
+
+			return (
+				parseFloat(value).toLocaleString("tr-TR", {
+					minimumFractionDigits: 2,
+					maximumFractionDigits: 4,
+				}) + "%"
+			);
+		}
+
+		return default_formatter(value, row, column, data);
+	},
+};
